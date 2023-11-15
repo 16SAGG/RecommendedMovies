417fb0f7d3cf66a5e7696ecb0c7446c974167419
Samuel <sagg160800@gmail.com>
2023-11-15 17:56:39 -0400

    'firstCommit'

---
+++ src/data/movies.js
@@ -0,0 +1,56 @@
+export const movies = [
+    {
+        id : "kiki-delivery-service",
+        title : "Kiki's Delivery Service",
+        image : "https://cdn.shopify.com/s/files/1/0618/8715/2293/products/kiki_hires_3.jpg?v=1675792088&width=800&height=450&crop=center",
+        synopsis : "In this heartwarming animated film from Studio Ghibli, 13-year-old Kiki embarks on a journey of self-discovery as she sets out to fulfill her dream of becoming a full-fledged witch. Guided by her wise black cat Jiji, Kiki ventures to the bustling seaside town of Koriko, where she establishes a unique delivery service using her magical flying abilities.",
+        director : "Hayao Miyazaki",
+        writer : "Hayao Miyazaki",
+        studio : "Studio Ghibli"
+    },
+    {
+        id : "howl-moving-castle",
+        title : "Howl's Moving Castle",
+        image : "https://cdn.shopify.com/s/files/1/0618/8715/2293/files/howl_hires_1.webp?v=1671141453&width=800&height=450&crop=center",
+        synopsis : "Howl's Moving Castle is a 2004 Japanese animated fantasy film written and directed by Hayao Miyazaki. Loosely based on the 1986 novel of the same name by British author Diana Wynne Jones, the film tells the story of Sophie, a young woman who is cursed by the Witch of the Waste to become an old woman. She flees to the Moving Castle, home to the wizard Howl, and becomes his assistant." ,
+        director : "Hayao Miyazaki",
+        writer : "Hayao Miyazaki",
+        studio : "Studio Ghibli"
+    },
+    {
+        id : "my-neighbor-totoro",
+        title : "My Neighbor Totoro",
+        image : "https://cdn.shopify.com/s/files/1/0618/8715/2293/files/totoro_hires_1.webp?v=1671142250&width=800&height=450&crop=center",
+        synopsis : "In 1958 Japan, university professor Tatsuo Kusakabe and his two daughters, Satsuki and Mei, move to an old house in the countryside to be closer to their hospitalized mother. While exploring the nearby woods, Mei encounters Totoro, a gigantic, friendly creature who resembles a cat mixed with a rabbit. Mei befriends Totoro and his smaller companions, and the two sisters soon find themselves enchanted by the magical world of the forest.",
+        director : "Hayao Miyazaki",
+        writer : "Hayao Miyazaki",
+        studio : "Studio Ghibli"
+    },
+    {
+        id : "spirited-away",
+        title : "Spirited Away",
+        image : "https://cdn.shopify.com/s/files/1/0618/8715/2293/products/spirited_hires_2_4a92150b-af0e-4b04-93f9-eb6ea978126b.jpg?v=1675796008&width=800&height=450&crop=center",
+        synopsis : "The story follows Chihiro Ogino, a sullen ten-year-old girl who, while moving to a new neighborhood, enters the world of kami (spirits of Japanese Shinto folklore). After her parents are turned into pigs by the witch Yubaba, Chihiro takes a job working in Yubaba's bathhouse to find a way to free herself and her parents and return to the human world.",
+        director : "Hayao Miyazaki",
+        writer : "Hayao Miyazaki",
+        studio : "Studio Ghibli"
+    },
+    {
+        id : "ponyo",
+        title : "Ponyo",
+        image : "https://cdn.shopify.com/s/files/1/0618/8715/2293/files/ponyo_hires_2.jpg?v=1676047969&width=800&height=450&crop=center",
+        synopsis : "The film is about a goldfish named Ponyo who escapes from the ocean and is helped by a five-year-old human boy, Sōsuke, after she is washed ashore while trapped in a glass jar. As they bond with each other, Ponyo desires to become a human girl, against the devastating circumstances brought about by her acquisition and use of magic.",
+        director : "Hayao Miyazaki",
+        writer : "Hayao Miyazaki",
+        studio : "Studio Ghibli"
+    },
+    {
+        id : "when-marnie-was-there",
+        title : "When Marnie Was There",
+        image : "https://cdn.shopify.com/s/files/1/0618/8715/2293/products/when-marnie-was-there-thumbnail_01a77806-c65d-457f-9eac-ba33269ff79a.jpg?v=1675276137&width=800&height=450&crop=center",
+        synopsis : "When Marnie Was There is a 2014 Japanese animated psychological drama film written and directed by Hiromasa Yonebayashi, produced by Studio Ghibli and released on July 19, 2014. Based on Joan G. Robinson's 1967 novel of the same name, the film follows the story of Anna Sasaki, a shy and introverted 12-year-old girl who is sent to stay with her relatives in the countryside to recover from asthma. While exploring the nearby marshes, Anna encounters Marnie, a mysterious and beautiful blonde girl who lives in an abandoned mansion. The two girls quickly form a close bond, but their friendship is complicated by Marnie's enigmatic nature and Anna's own struggles with loneliness and isolation.",
+        director : "Hiromasa Yonebayashi",
+        writer : "Hiromasa Yonebayash & Keiko Niwa",
+        studio : "Studio Ghibli"
+    }
+]
