fa4c49a6b3cd6ce4be965961672c1fa96cbc135f
Samuel <sagg160800@gmail.com>
2024-02-06 11:40:24 -0400

    githubPagesDeploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://16sagg.github.io',
+  base: '/my-repo',
   integrations: [tailwind()],
   output: 'server',
   adapter: node({
